0373559814c20ec056f53dc668b73ddb6f59a92c
Dias <166080835+odiasdev@users.noreply.github.com>
2025-06-17 09:11:17 -0300

    Update settings.py

--- memoria/settings.py
+++ memoria/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dovovv21+r39svf+5(-y1y-i%#dwnm6x(05_(dn^y$2c(sa+$r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['memorianacional-production.up.railway.app']
 
